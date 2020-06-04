ef66498df80ee4c7613e4cebf522e94db5ec08dd
Christian Passold <passold.christian@hotmail.com>
2020-06-04 20:09:02 -0300

    ajustes

--- CODIGO-FONTE/as_aventuras_de_bilu_/js/snake.js
+++ CODIGO-FONTE/as_aventuras_de_bilu_/js/snake.js
@@ -1,174 +1,174 @@
 class Snake {
-  constructor(ctx, name, id, mainColor, max) {
-    this.ctx = ctx;
-    this.name = name;
-    this.id = id;
-    this.score = 0;
-    this.force = 0;
-    this.state = 0;
-    this.headType = ut.random(0, 2);
+    constructor(ctx, name, id, mainColor, max) {
+        this.ctx = ctx;
+        this.name = name;
+        this.id = id;
+        this.score = 0;
+        this.force = 0;
+        this.state = 0;
+        this.headType = ut.random(0, 2);
 
-    this.enemyImage = new Image();
-    this.enemyImage.src = IMAGE_ENEMY_SRC;
+        this.enemyImage = new Image();
+        this.enemyImage.src = IMAGE_ENEMY_SRC;
 
-    //
-    this.pos = new Point(game.SCREEN_SIZE.x / 2, game.SCREEN_SIZE.y / 2);
-    this.velocity = new Point(0, 0); //arbitary point
-    this.angle = ut.random(0, Math.PI);
+        //
+        this.pos = new Point(game.SCREEN_SIZE.x / 2, game.SCREEN_SIZE.y / 2);
+        this.velocity = new Point(0, 0); //arbitary point
+        this.angle = ut.random(0, Math.PI);
 
-    this.length = 10;
-    this.MAXSIZE = max ? max : 100;
-    this.size = 7;
-    this.countFoodDie = 0;
+        this.length = 10;
+        this.MAXSIZE = max ? max : 100;
+        this.size = 7;
+        this.countFoodDie = 0;
 
-    this.arr = [];
-    this.arr.push(new Point(game.SCREEN_SIZE.x / 2, game.SCREEN_SIZE.y / 2));
-    for (var i = 1; i < this.length; i++) {
-      this.arr.push(new Point(this.arr[i - 1].x, this.arr[i - 1].y));
-    }
-
-    if (!this.isAi()) {
-      document.onmousemove = (event) => {
-        this.arr[0].x = event.clientX;
-        this.arr[0].y = event.clientY;
-        this.move();
-      };
-    }
-  }
-
-  isAi() {
-    return this instanceof SnakeAi;
-  }
-
-  drawHeadOneEye() {
-    var x = this.arr[0].x;
-    var y = this.arr[0].y;
-
-    if (this.isAi()) {
-      this.ctx.drawImage(
-        this.enemyImage,
-        0,
-        0,
-        IMAGE_ENEMY_WIDTH,
-        IMAGE_ENEMY_HEIGHT,
-        x,
-        y,
-        IMAGE_ENEMY_WIDTH,
-        IMAGE_ENEMY_HEIGHT
-      );
-    }
-  }
-
-  move() {
-    this.velocity.x = this.force * Math.cos(this.angle);
-    this.velocity.y = this.force * Math.sin(this.angle);
-
-    //magic
-    var d = this.size / 2;
-    if (this.isAi()) {
-      for (var i = this.length - 1; i >= 1; i--) {
-        this.arr[i].x = this.arr[i - 1].x - d * Math.cos(this.angle);
-        this.arr[i].y = this.arr[i - 1].y - d * Math.sin(this.angle);
-      }
-    }
-
-    this.pos.x += this.velocity.x;
-    this.pos.y += this.velocity.y;
-    this.drawHeadOneEye();
-    this.checkCollissionFood();
-    this.checkCollissionSnake();
-    this.checkBoundary();
-  }
-
-  checkBoundary() {
-    //left
-    if (this.arr[0].x < game.world.x) {
-      this.pos.x = game.world.x + this.size * 2;
-      this.velocity.x *= -1;
-      this.angle = Math.PI - this.angle;
-    }
-
-    //right
-    else if (this.arr[0].x > game.world.x + game.WORLD_SIZE.x) {
-      this.pos.x = game.world.x + game.WORLD_SIZE.x - this.size * 2;
-      this.velocity.x *= -1;
-      this.angle = Math.PI - this.angle;
-    }
-
-    //up
-    else if (this.arr[0].y < game.world.y) {
-      this.pos.y = game.world.y + this.size * 2;
-      this.velocity.y *= -1;
-      this.angle = Math.PI - this.angle;
-    }
-
-    //down
-    else if (this.arr[0].y > game.world.y + game.WORLD_SIZE.y) {
-      this.pos.y = game.world.y + game.WORLD_SIZE.y - this.size * 2;
-      this.velocity.y *= -1;
-      this.angle = Math.PI - this.angle;
-    }
-  }
-
-  //check snake and food collission
-  checkCollissionFood() {
-    var x = this.arr[0].x;
-    var y = this.arr[0].y;
-    for (var i = 0; i < game.foods.length; i++) {
-      if (
-        ut.cirCollission(
-          x,
-          y,
-          this.size,
-          game.foods[i].pos.x,
-          game.foods[i].pos.y,
-          game.foods[i].size
-        )
-      ) {
-        game.foods[i].die();
-        this.countFoodDie++;
-        this.addScore();
-      }
-    }
-  }
-
-  checkCollissionSnake() {
-    var x = this.arr[0].x;
-    var y = this.arr[0].y;
-    for (var i = 0; i < game.snakes.length; i++) {
-      var s = game.snakes[i];
-      if (s !== this) {
-        for (var j = 0; j < game.snakes[i].arr.length; j += 2) {
-          if (
-            ut.cirCollission(x, y, this.size, s.arr[j].x, s.arr[j].y, s.size)
-          ) {
-            if (this.size > s.size) {
-              s.die();
-            } else {
-              this.die();
-            }
-          }
+        this.arr = [];
+        this.arr.push(new Point(game.SCREEN_SIZE.x / 2, game.SCREEN_SIZE.y / 2));
+        for (var i = 1; i < this.length; i++) {
+            this.arr.push(new Point(this.arr[i - 1].x, this.arr[i - 1].y));
         }
-      }
+
+        if (!this.isAi()) {
+            document.onmousemove = (event) => {
+                this.arr[0].x = event.clientX;
+                this.arr[0].y = event.clientY;
+                this.move();
+            };
+        }
     }
-  }
 
-  addScore() {
-    this.length++;
-    this.score++;
-    this.arr.push(new Point(-100, -100));
-  }
+    isAi() {
+        return this instanceof SnakeAi;
+    }
 
-  changeAngle(angle) {
-    this.angle = angle;
-  }
+    drawHeadOneEye() {
+        var x = this.arr[0].x;
+        var y = this.arr[0].y;
 
-  die() {
-    this.state = 1;
-    for (var i = 0; i < this.arr.length; i += 3)
-      game.foods.push(new Food(game.ctxFood, 0, 0));
+        if (this.isAi()) {
+            this.ctx.drawImage(
+                this.enemyImage,
+                0,
+                0,
+                IMAGE_ENEMY_WIDTH,
+                IMAGE_ENEMY_HEIGHT,
+                x,
+                y,
+                IMAGE_ENEMY_WIDTH,
+                IMAGE_ENEMY_HEIGHT
+            );
+        }
+    }
 
-    var index = game.snakes.indexOf(this);
-    game.snakes.splice(i, 1);
-  }
+    move() {
+        this.velocity.x = this.force * Math.cos(this.angle);
+        this.velocity.y = this.force * Math.sin(this.angle);
+
+        //magic
+        var d = this.size / 2;
+        if (this.isAi()) {
+            for (var i = this.length - 1; i >= 1; i--) {
+                this.arr[i].x = this.arr[i - 1].x - d * Math.cos(this.angle);
+                this.arr[i].y = this.arr[i - 1].y - d * Math.sin(this.angle);
+            }
+        }
+
+        this.pos.x += this.velocity.x;
+        this.pos.y += this.velocity.y;
+        this.drawHeadOneEye();
+        this.checkCollissionFood();
+        this.checkCollissionSnake();
+        this.checkBoundary();
+    }
+
+    checkBoundary() {
+        //left
+        if (this.arr[0].x < game.world.x) {
+            this.pos.x = game.world.x + this.size * 2;
+            this.velocity.x *= -1;
+            this.angle = Math.PI - this.angle;
+        }
+
+        //right
+        else if (this.arr[0].x > game.world.x + game.WORLD_SIZE.x) {
+            this.pos.x = game.world.x + game.WORLD_SIZE.x - this.size * 2;
+            this.velocity.x *= -1;
+            this.angle = Math.PI - this.angle;
+        }
+
+        //up
+        else if (this.arr[0].y < game.world.y) {
+            this.pos.y = game.world.y + this.size * 2;
+            this.velocity.y *= -1;
+            this.angle = Math.PI - this.angle;
+        }
+
+        //down
+        else if (this.arr[0].y > game.world.y + game.WORLD_SIZE.y) {
+            this.pos.y = game.world.y + game.WORLD_SIZE.y - this.size * 2;
+            this.velocity.y *= -1;
+            this.angle = Math.PI - this.angle;
+        }
+    }
+
+    //check snake and food collission
+    checkCollissionFood() {
+        var x = this.arr[0].x;
+        var y = this.arr[0].y;
+        for (var i = 0; i < game.foods.length; i++) {
+            if (
+                ut.cirCollission(
+                    x,
+                    y,
+                    this.size,
+                    game.foods[i].pos.x,
+                    game.foods[i].pos.y,
+                    game.foods[i].size
+                )
+            ) {
+                game.foods[i].die();
+                this.countFoodDie++;
+                this.addScore();
+            }
+        }
+    }
+
+    checkCollissionSnake() {
+        var x = this.arr[0].x;
+        var y = this.arr[0].y;
+        for (var i = 0; i < game.snakes.length; i++) {
+            var s = game.snakes[i];
+            if (s !== this) {
+                for (var j = 0; j < game.snakes[i].arr.length; j += 2) {
+                    if (
+                        ut.cirCollission(x, y, this.size, s.arr[j].x, s.arr[j].y, s.size)
+                    ) {
+                        if (this.size > s.size) {
+                            s.die();
+                        } else {
+                            this.die();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    addScore() {
+        this.length++;
+        this.score++;
+        this.arr.push(new Point(-100, -100));
+    }
+
+    changeAngle(angle) {
+        this.angle = angle;
+    }
+
+    die() {
+        this.state = 1;
+        for (var i = 0; i < this.arr.length; i += 3)
+            game.foods.push(new Food(game.ctxFood, 0, 0));
+
+        var index = game.snakes.indexOf(this);
+        game.snakes.splice(i, 1);
+    }
 }
